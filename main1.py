532d125efe29435d33bd2b8c69e02478b3ff6952
mistscat <whitesnoooy@gmail.com>
2021-03-26 18:22:16 +0300

    upload

--- main1.py
+++ main1.py
@@ -4,8 +4,51 @@ from discord.ext import commands
 from functions import embed
 
 
+
+
 intents = discord.Intents().all()
-client = commands.Bot(command_prefix=PREFIX)
+client = commands.Bot(command_prefix=PREFIX,case_insensitive=True  )
+
+#welcome_message
+
+# welcome
+@client.event
+async def on_member_join(member):
+    print(f"{member.name} has joined Welcome! 😊 ")
+    channel = discord.utils.get(member.guild.text_channels, name="welcome and goodbyes")
+    await channel.send(f"{member.mention} has joined! 😊")
+
+
+
+#modeeration
+
+
+# kick
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    if ctx.author in ctx.guild.get_role(825026101528756224).members:
+        await member.kick(reason=reason)
+        await ctx.send(f"{member.mention} has been kicked")
+    else:
+        await ctx.reply("You are not allowed to use that command")
+
+
+# ban
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    if ctx.author in ctx.guild.get_role(825026101528756224).members:
+        await member.ban(reason=reason)
+    else:
+        await ctx.reply("You are not allowed to use that command ")
+
+
+#purge
+@client.command(aliases=["r", "purge","delete"])
+@commands.has_permissions(manage_messages=True)
+async def remove(ctx, amount : int):
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f'Purged {amount} messages')
+
 
 # ping
 @client.command()
@@ -22,7 +65,7 @@ async def ping(ctx):
 async def on_ready():
     print('SUCCESSFULLY LOGGED IN AS', client.user.name.upper())
 
-    await client.get_channel(823833801981165611).send(f"{client.user.mention} Im AWAKE!!")
+    await client.get_channel(824596004242194485).send(f"{client.user.mention} Im AWAKE!!")
 
 
 
